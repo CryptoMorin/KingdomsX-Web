b95ebbb240521c47828780ec2af42f660f9d644d
AfkF24 <us.faris.ajanovic@gmail.com>
2026-06-05 22:55:57 +0200

    Use deep discord link for 'Get Dev Builds' button

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,7 @@ export const SITE = {
   assets: trimTrailingSlash(import.meta.env.PUBLIC_ASSETS_BASE ?? "/assets"),
   wiki: "https://wiki.kingdomsx.com",
   discord: "https://discord.kingdomsx.com",
+  discordDevBuilds: "discord://-/channels/429132410748141579/430983456932102154",
   spigot: "https://www.spigotmc.org/resources/77670/",
   modrinth: "https://modrinth.com/plugin/kingdomsx/",
   github: "https://github.com/CryptoMorin/KingdomsX",
